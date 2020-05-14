fc3b7d4d8f8f6cd6a9a4453380ef7135d6c13f99
Luigi Vitelli <luigi.vitelli23@gmail.com>
2020-05-14 11:27:26 +0200

    1 - initial commit

---
+++ app/src/main/java/it/flowing/app/ui/MainActivity.kt
@@ -0,0 +1,13 @@
+package it.flowing.app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import it.flowing.app.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
